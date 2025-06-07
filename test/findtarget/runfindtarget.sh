ca1c2a9a240759628a2b8079e11e40445ae6651c
liuyilei8969 <ylliu8969@gmail.com>
2025-06-07 15:24:30 +0800

    Create runfindtarget.sh

---
+++ test/findtarget/runfindtarget.sh
@@ -0,0 +1,7 @@
+python src/SPONGE/findtarget.py \
+--rmats test/findtarget/input/shQKI_rmats.txt \
+--clip_peaks test/findtarget/input/QKI_CLIP_sig_peaks.bed \
+--ref_genome REF_GENOME \
+--rbp_motif data/Motif.txt \
+--rbp_name QKI \
+--output test/findtarget/output
