5607a6bcec376f5818a6ad44d87816367bf34b7c
liuyilei8969 <ylliu8969@gmail.com>
2025-06-12 20:06:48 +0800

    Update __init__.py

---
+++ src/SPONGE/__init__.py
@@ -0,0 +1,9 @@
+"""
+SPONGE: Splicing-factor activity Prediction based On Network and Global splicing Events
+
+This module contains core functions for:
+- Finding splicing targets
+- Constructing splicing regulatory networks
+- Estimating RBP activity
+"""
+__version__ = "0.1.0"
