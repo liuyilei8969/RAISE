caf65c9691532b2447434b531f5ac39c29cafad7
liuyilei8969 <ylliu8969@gmail.com>
2025-06-12 16:02:41 +0800

    Create runcalculate_activity.sh

---
+++ test/calculate_activity/runcalculate_activity.sh
@@ -0,0 +1,4 @@
+python calculate_activity.py 
+ --diffAS test/calculate_activity/input/HLE-B3_shQKI_rmats.txt
+ --gexf data/SF-exon_network.gexf
+ --out test/calculate_activity/output/HLE-B3_activity.txt
