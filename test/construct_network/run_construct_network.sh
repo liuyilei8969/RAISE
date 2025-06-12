2805dbf0b5c8ba845b1a58a455e9b1ba844cd4f5
liuyilei8969 <ylliu8969@gmail.com>
2025-06-12 18:51:23 +0800

    Update run_construct_network.sh

--- test/construct_network/run_construct_network.sh
+++ test/construct_network/run_construct_network.sh
@@ -1,2 +1,5 @@
-python src/SPONGE/construct_network/construct_network.py
- 
+python src/SPONGE/construct_network/construct_network.py \
+ --Target_dir test/construct_network/input/Target_Result/ \
+ --threshold 0.6 \
+ --DE_dir test/construct_network/input/DE/ \
+ --output test/construct_network/output/test_network.gexf
