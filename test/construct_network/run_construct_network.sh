ec3bd3914fa78acf82598024b1e3c55087a33d9c
liuyilei8969 <ylliu8969@gmail.com>
2025-06-14 19:45:39 +0800

    Update run_construct_network.sh

--- test/construct_network/run_construct_network.sh
+++ test/construct_network/run_construct_network.sh
@@ -1,4 +1,4 @@
-python src/SPONGE/construct_network/construct_network.py \
+python src/SPONGE/construct_network.py \
  --Target_dir test/construct_network/input/Target_Result/ \
  --threshold 0.6 \
  --DE_dir test/construct_network/input/DE/ \
