3484b4792a462d7f00919ffd6855bb13a1d40994
liuyilei8969 <ylliu8969@gmail.com>
2025-06-14 19:46:03 +0800

    Update run_calculate_activity.sh

--- test/calculate_activity/run_calculate_activity.sh
+++ test/calculate_activity/run_calculate_activity.sh
@@ -1,4 +1,4 @@
-python calculate_activity.py \
+python src/SPONGE/calculate_activity.py \
  --diffAS test/calculate_activity/input/HLE-B3_shQKI_rmats.txt \
  --network data/SF-exon_network.gexf \
  --out test/calculate_activity/output/HLE-B3_activity.txt
