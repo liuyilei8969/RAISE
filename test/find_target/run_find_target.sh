1e57f5e7f684b8dc8e6e9149ba1110b3b04de8d2
liuyilei8969 <ylliu8969@gmail.com>
2025-06-14 19:59:31 +0800

    Update run_find_target.sh

--- test/find_target/run_find_target.sh
+++ test/find_target/run_find_target.sh
@@ -1,10 +1,10 @@
 python src/SPONGE/find_target.py \
---rmats test/findtarget/input/shQKI_rmats.txt \
---clip_peaks test/findtarget/input/QKI_CLIP_sig_peaks.bed \
+--rmats test/find_target/input/shQKI_rmats.txt \
+--clip_peaks test/find_target/input/QKI_CLIP_sig_peaks.bed \
 --ref_genome REF_GENOME \
 --rbp_motif data/Motif.txt \
 --cell_line HepG2 \
 --rbp QKI \
---output test/findtarget/output \
+--output test/find_target/output \
 --max_iter 1000 \
 --tol 1e-4
